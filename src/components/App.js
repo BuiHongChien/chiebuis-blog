db369dced1b1e55f91b94b3a411704cf10ea00e6
Bhchien211 <BuiHongChien@users.noreply.github.com>
2020-12-03 22:05:53 +0300

    add botchat

--- src/components/App.js
+++ src/components/App.js
@@ -18,16 +18,31 @@ export const about = [
 export const interests = ["travel", "drawing", "books", "music", "fashion"];
 export const plans = ["this-week", "next-week", "this-month"];
 export const goals = ["toeic", "yoga"];
+export const randomMsg = {
+  adjs: ["Fat", "Black", "White", "Small", "Pretty", "Brown", "Big"],
+  nouns: ["Penguin", "Cat", "Swan", "Peacock", "Bear", "Giraffe", "Elephant"],
+  verbs: [
+    "is sleeping",
+    "is eating",
+    "is running",
+    "is walking",
+    "is flying",
+    "is sitting",
+    "is chewing",
+    "is dancing",
+    "is typing",
+  ],
+};
 
 const App = () => {
   return (
-      <BrowserRouter>
-        <Route path="/" exact component={Home} />
-        <Route path="/about" exact component={About} />
-        <Route path="/plans" exact component={Plans} />
-        <Route path="/goals" exact component={Goals} />
-        <Route path="/interests" exact component={Interests} />
-      </BrowserRouter>
+    <BrowserRouter>
+      <Route path="/" exact component={Home} />
+      <Route path="/about" exact component={About} />
+      <Route path="/plans" exact component={Plans} />
+      <Route path="/goals" exact component={Goals} />
+      <Route path="/interests" exact component={Interests} />
+    </BrowserRouter>
   );
 };
 
--- src/components/pages/home/Home.js
+++ src/components/pages/home/Home.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import ReactHtmlParser from "react-html-parser";
 
 import avatar from "../../../images/avatar.jpg";
 import goals from "../../../images/goals.png";
@@ -7,13 +8,21 @@ import about from "../../../images/about.png";
 import plans from "../../../images/plans.png";
 import interests from "../../../images/interests.png";
 import { fb, vk, ins, github } from "../links";
+import { randomMsg } from "../../App";
 
 import "./Home.css";
+import { render } from "@testing-library/react";
 
+let perMsgs = [];
+let robotMsgs = [];
 
 const Home = () => {
   const [showMenu, setShowMenu] = useState(false);
   const [showContact, setShowContact] = useState(false);
+  const [showAvatar, setShowAvatar] = useState(false);
+  const [showChatBot, setShowChatBot] = useState(false);
+  const [inputMessage, setInputMessage] = useState("");
+  const [running, setRunning] = useState(false);
 
   const renderedMenu = () => {
     return (
@@ -100,6 +109,21 @@ const Home = () => {
     );
   };
 
+  const renderedAvatar = () => {
+    return (
+      <div className="dark-overlay">
+        <div className="show-avatar">
+          <img src={avatar} alt="avatar" className="avatar-open" />
+          <button
+            className="btn-contact contact-close"
+            onClick={() => setShowAvatar(false)}
+          >
+            X
+          </button>
+        </div>
+      </div>
+    );
+  };
 
   const renderedContactWindow = () => {
     return (
@@ -118,37 +142,37 @@ const Home = () => {
             src={avatar}
             alt="avatar"
             className="contact-avatar"
-            onClick={()=>window.open(avatar)}
+            onClick={() => setShowAvatar(true)}
           ></img>
 
           <ul>
             <li>
-              <i class="fab fa-google-plus"></i>chienbui211@gmail.com
+              <i className="fab fa-google-plus"></i>chienbui211@gmail.com
             </li>
             <li>
-              <i class="fas fa-phone-square-alt"></i>+(79) 313 557 094
+              <i className="fas fa-phone-square-alt"></i>+(79) 313 557 094
             </li>
             <li>
-              <i class="fab fa-facebook-square"></i>
+              <i className="fab fa-facebook-square"></i>
               <a href={fb} target="_blank">
                 ChienBui
               </a>
             </li>
 
             <li>
-              <i class="fa fa-instagram"></i>
+              <i className="fa fa-instagram"></i>
               <a href={ins} target="_blank">
                 buichien.211
               </a>
             </li>
             <li>
-              <i class="fa fa-github"></i>
+              <i className="fa fa-github"></i>
               <a href={github} target="_blank">
                 BuiHongChien
               </a>
             </li>
             <li>
-              <i class="fab fa-vk"></i>
+              <i className="fab fa-vk"></i>
               <a href={vk} target="_blank">
                 BuiChien
               </a>
@@ -159,16 +183,130 @@ const Home = () => {
     );
   };
 
+  const addPerMsg = (id) => {
+    const div = document.createElement("div");
+    div.innerHTML = `
+                <div class="message-per-container id-${id}">
+                  <button class="btn-del del-${id}">
+                  X
+                  </button>
+                  <div class="message message-per">${perMsgs[id]}</div>
+                </div>`;
+    document.querySelector(".messages-list").appendChild(div);
+
+    const del = document.querySelector(`.id-${id}`);
+    document
+      .querySelector(`.del-${id}`)
+      .addEventListener("click", function (e) {
+        e.preventDefault();
+        del.style.display = "none";
+      });
+  };
+
+  const addRobotMsg = (id) => {
+    let div = document.createElement("div");
+    div.innerHTML = `
+              <div class="message-robot-container robot-${id}">
+                <div class="message message-robot">${robotMsgs[id]}</div>
+              </div>
+    `;
+    document.querySelector(".messages-list").appendChild(div);
+    setRunning(false);
+  };
+
+  const btnSendOnClick = (e) => {
+    e.preventDefault();
+    if (running) return;
+
+    perMsgs.push(inputMessage);
+    setInputMessage("");
+
+    setRunning(true);
+
+    addPerMsg(perMsgs.length - 1);
+
+    const adj =
+      randomMsg.adjs[Math.floor(Math.random() * randomMsg.adjs.length)];
+    const noun =
+      randomMsg.nouns[Math.floor(Math.random() * randomMsg.nouns.length)];
+    const verb =
+      randomMsg.verbs[Math.floor(Math.random() * randomMsg.verbs.length)];
+    const robotMsg = "A " + adj + " " + noun + " " + verb;
+    robotMsgs.push(robotMsg);
+    window.setTimeout(addRobotMsg, 1000, robotMsgs.length - 1);
+  };
+
+  const renderedChatBot = () => {
+    return (
+      <div className="chatbot-container">
+        <div className="contact-header">
+          <i className="robot fas fa-robot"></i>
+          <button
+            className="btn-contact contact-close"
+            onClick={() => setShowChatBot(false)}
+          >
+            X
+          </button>
+        </div>
+
+        <div className="message-content">
+          <div className="messages">
+            <div className="messages-list"></div>
+          </div>
+          <div className="input-blank">
+            <input
+              autoFocus
+              type="text"
+              className="message-field"
+              placeholder="Enter a message..."
+              value={inputMessage}
+              onKeyDown={(e) => {
+                if (e.key === "Enter") {
+                  if (inputMessage) btnSendOnClick(e);
+                  else e.preventDefault();
+                }
+              }}
+              onChange={(e) => {
+                setInputMessage(e.target.value);
+              }}
+            ></input>
+            <button
+              className="btn-send"
+              onClick={inputMessage ? btnSendOnClick : null}
+            >
+              <i className="fas fa-paper-plane"></i>
+            </button>
+          </div>
+        </div>
+      </div>
+    );
+  };
+
   return (
-    <div className="home">
-      {showMenu ? renderedMenu() : renderedHome()}
-      <button
-        className="btn-contact contact-open"
-        onClick={() => setShowContact(true)}
-      >
-        Contact Me
-      </button>
-      {showContact ? renderedContactWindow() : null}
+    <div>
+      <div className="home">
+        {showMenu ? renderedMenu() : renderedHome()}
+
+        <div className="window-container">
+          <button
+            className="btn-contact contact-open"
+            onClick={() => setShowChatBot(true)}
+          >
+            <i className="far fa-comment-dots"></i>
+          </button>
+          <button
+            className="btn-contact contact-open"
+            onClick={() => setShowContact(true)}
+          >
+            Contact Me
+          </button>
+
+          {showContact ? renderedContactWindow() : null}
+          {showChatBot ? renderedChatBot() : null}
+        </div>
+      </div>
+
+      {showAvatar ? renderedAvatar() : null}
     </div>
   );
 };
